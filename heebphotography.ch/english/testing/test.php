96aadd7946b36c5e6a280c46f6da5cb65ff58b61
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-13 18:35:34 +0200

    Update test.php

--- heebphotography.ch/english/testing/test.php
+++ heebphotography.ch/english/testing/test.php
@@ -5,26 +5,24 @@
 </head>
 
 <body>
-    <h1>
-        <?php
-        echo "Hello World!";
-        echo "\r\n";
-        $Name = "Dominik Schwaiger";
-        $Age = 16;
-        $Mood = "Tired";
-        echo $Name . "";
-        echo $Mood . "\r\n";
-        echo $Age . "\r\n";
-        define("Constant", "This constant is very nice");
-        echo Constant . "\r\n";
-        echo "\r\n\r\n";
-        echo $Name;
-        echo "\r\n";
-        echo $$Name;
-        echo "\r\n";
-        echo $$$Name;
-        ?>
-    </h1>
+    <?php
+    echo "Hello World!";
+    echo "\r\n";
+    $Name = "Dominik Schwaiger";
+    $Age = 16;
+    $Mood = "Tired";
+    echo $Name . "";
+    echo $Mood . "\r\n";
+    echo $Age . "\r\n";
+    define("Constant", "This constant is very nice");
+    echo Constant . "\r\n";
+    echo "\r\n\r\n";
+    echo $Name;
+    echo "\r\n";
+    echo $$Name;
+    echo "\r\n";
+    echo $ $$Name;
+    ?>
     <!-- <script language="php">
     echo "Hello World!";
     </script> -->
