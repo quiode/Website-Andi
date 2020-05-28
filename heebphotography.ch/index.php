4fd170767aa74a3a7953df473a589b90f903bb09
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-28 22:12:33 +0200

    Update index.php

--- heebphotography.ch/index.php
+++ heebphotography.ch/index.php
@@ -7,15 +7,24 @@
 
 <body>
     <ul>
-        <li>
+        <!-- <li>
             <a href="../public/english" id="english">English</a>
-        </li>
-        <li>
+        </li> -->
+        <!-- <li>
             <a href="../public/deutsch" id="deutsch">Deutsch</a>
+        </li> -->
+        <li>
+            <?php
+            echo "<a href=\"en.";
+            echo $_SERVER['HTTP_HOST'];
+            echo "\" id =\"english\">English</a>>"
+            ?>
         </li>
         <li>
             <?php
+            echo "<a href=\"de.";
             echo $_SERVER['HTTP_HOST'];
+            echo "\" id =\"deutsch\">Deutsch</a>>"
             ?>
         </li>
     </ul>
