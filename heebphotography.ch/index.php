fd8e2908bb51f598fc4b9b8519fc997336e1ee87
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-28 21:59:19 +0200

    css stylesheet for language selector

--- heebphotography.ch/index.php
+++ heebphotography.ch/index.php
@@ -2,15 +2,16 @@
 
 <head>
     <title>Language | Wildlifephotography Andreas Heeb</title>
+    <link rel="stylesheet" href="stylesheet.css">
 </head>
 
 <body>
     <ul>
         <li>
-            <a>English</a>
+            <a id="english">English</a>
         </li>
         <li>
-            <a>Deutsch</a>
+            <a id="german">Deutsch</a>
         </li>
     </ul>
 </body>
