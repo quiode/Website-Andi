e57bd5257042d0d9b1b5b41466d9d5a83665fafa
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-29 23:38:28 +0200

    changed flexbox
    moved from a column approach to just a listing of all elements, testing if it work out, if now, I'm switching back

--- heebphotography.ch/public/english/gallery/index.php
+++ heebphotography.ch/public/english/gallery/index.php
@@ -16,70 +16,11 @@
     $images = scandir("images", 1);
     unset($images[sizeof($images)-1]);
     unset($images[sizeof($images)-1]);
-    $image_column_1 = array();
-    $image_column_2 = array();
-    $image_column_3 = array();
-    $image_column_4 = array();
-    
-    $i = 0;
-    $c = 0;
-    while ($i <= sizeof($images)) {
-        $image_column_1[$c] = $images[$i++];
-        if ($i >= sizeof($images)) {
-        break;
-        }
-        $image_column_2[$c] = $images[$i++];
-        if ($i >= sizeof($images)) {
-        break;
-        }
-        $image_column_3[$c] = $images[$i++];
-        if ($i >= sizeof($images)) {
-        break;
-        }
-        $image_column_4[$c++] = $images[$i++];
-    }
-        ?>
-        <div class="row">
-            <div class="column">
-                <?php
-                foreach ($image_column_1 as $image) {
-                        echo "<img src=\"images/";
-                        echo $image;
-                        echo "\" class=\"image\">";
-                }
-                ?>
-            </div>
-
-            <div class="column">
-                <?php
-                foreach ($image_column_2 as $image) {
-                        echo "<img src=\"images/";
-                        echo $image;
-                        echo "\" class=\"image\">";
-                }
-                ?>
-            </div>
-
-            <div class="column">
-                <?php
-                foreach ($image_column_3 as $image) {
-                        echo "<img src=\"images/";
-                        echo $image;
-                        echo "\" class=\"image\">";
-                }
-                ?>
-            </div>
-
-            <div class="column">
-                <?php
-                foreach ($image_column_4 as $image) {
-                        echo "<img src=\"images/";
-                        echo $image;
-                        echo "\" class=\"image\">";
-                }
-                ?>
-            </div>
-        </div>
+    foreach ($images as $image){
+        echo "<img src=\"images/";
+        echo $image;
+        echo "\" class=\"image\">";}
+    ?>
     </div>
 </body>
 
