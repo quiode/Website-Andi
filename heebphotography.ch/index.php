b4f73e66b206b2b5cdbd5174832ab4bf560c9e0d
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-30 00:23:49 +0200

    Update index.php

--- heebphotography.ch/index.php
+++ heebphotography.ch/index.php
@@ -4,6 +4,7 @@
 <head>
     <title>Language | Wildlifephotography Andreas Heeb</title>
     <link rel="stylesheet" href="stylesheet.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
