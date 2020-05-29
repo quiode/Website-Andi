03b95630095a7e3eebc484cbf7a28e824e40dd74
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-29 16:42:37 +0200

    stylesheet

--- heebphotography.ch/public/english/gallery/index.php
+++ heebphotography.ch/public/english/gallery/index.php
@@ -4,19 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet.css">
     <title>Gallery | Wildlifephotography Andreas Heeb</title>
 </head>
 
 <body>
     <nav>
     </nav>
-    <article>
+    <article id="gallery">
         <?php
     $images = scandir("images", 1);
     unset($images[sizeof($images)-1]);
     unset($images[sizeof($images)-1]);
     foreach ($images as $image){
-        echo "<section>";
+        echo "<section class=\"image\">";
 
         echo "<img src=\"images/";
         echo $image;
@@ -24,7 +25,7 @@
 
         echo "</section>";
     }
-    ?>
+        ?>
     </article>
 </body>
 
