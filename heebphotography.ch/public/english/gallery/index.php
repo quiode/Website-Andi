522acd6b60dd7521e396e6e1a8a9a88b506174f2
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-29 12:02:50 +0200

    image_filename_sorting_1

--- heebphotography.ch/public/english/gallery/index.php
+++ heebphotography.ch/public/english/gallery/index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gallery | Wildlifephotography Andreas Heeb</title>
 </head>
+
 <body>
-    
+    <?php
+    $images = scandir("/./images/", 1);
+    echo $images;
+    ?>
 </body>
+
 </html>
