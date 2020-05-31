129c2094b941b9982e104a47ba560848525154af
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-31 17:57:09 +0200

    navigation stuff

--- heebphotography.ch/public/english/templates/navigationbar.php
+++ heebphotography.ch/public/english/templates/navigationbar.php
@@ -14,13 +14,15 @@
         <div class="bar2"></div>
         <div class="bar3"></div>
     </div>
-    <div id="navigation">
-        <?php
+    <div id="navigation-overlay">
+        <div id="navigation-content">
+            <?php
         echo "<a class=\"links\" href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "gallery/" . "\">Gallery</a>";
         echo "<a class=\"links\" href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "about me/" . "\">About me</a>";
         echo "<a class=\"links\" href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "linktree/" . "\">Linktree</a>";
         echo "<a class=\"links\" href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "contact/" . "\">Contact</a>";
         echo "<a class=\"links\" href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "impressum/" . "\">Impressum</a>";
         ?>
+        </div>
     </div>
 </nav>
