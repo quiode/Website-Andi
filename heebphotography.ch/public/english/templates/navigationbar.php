11a3088da0dc508298b6bdbfcdddedc75d8b8cb1
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-31 17:02:15 +0200

    navigation stuff

--- heebphotography.ch/public/english/templates/navigationbar.php
+++ heebphotography.ch/public/english/templates/navigationbar.php
@@ -13,10 +13,10 @@
         <div class="bar2"></div>
         <div class="bar3"></div>
     </div>
-    <div id="">
+    <div id="navigation_bar">
         <ul>
             <?php
-        echo "<li class=\"links\"><a  href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "gallery/" . "\">Gallery</a></li>";
+        echo "<li class=\"links\"><a href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "gallery/" . "\">Gallery</a></li>";
         echo "<li class=\"links\"><a href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "about me/" . "\">About me</a></li>";
         echo "<li class=\"links\"><a href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "linktree/" . "\">Linktree</a></li>";
         echo "<li class=\"links\"><a href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "contact/" . "\">Contact</a></li>";
