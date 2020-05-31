7c0e45fbf29ee0b10f4bd9c30a87aef7fb649394
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-31 18:10:37 +0200

    navigation stuff

--- heebphotography.ch/public/english/templates/navigationbar.js
+++ heebphotography.ch/public/english/templates/navigationbar.js
@@ -5,11 +5,13 @@ function navigation_button(button) {
         // alert("Going Down");
         navigation.style.height = "100%";
         document.getElementById("b1").style.backgroundColor = "rgb(255,255,255)"
+        document.getElementById("b2").style.backgroundColor = "rgb(255,255,255)"
         document.getElementById("b3").style.backgroundColor = "rgb(255,255,255)"
     } else {
         // alert("Going Up");
         navigation.style.height = "0%";
         document.getElementById("b1").style.backgroundColor = "rgb(0,0,0)"
+        document.getElementById("b2").style.backgroundColor = "rgb(0,0,0)"
         document.getElementById("b3").style.backgroundColor = "rgb(0,0,0)"
     }
   } 
