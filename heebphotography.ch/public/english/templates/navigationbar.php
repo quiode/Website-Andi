52bc90706fc16d4c4ead0083d3bd3e7031cd27cc
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-31 16:01:39 +0200

    Update navigationbar.php

--- heebphotography.ch/public/english/templates/navigationbar.php
+++ heebphotography.ch/public/english/templates/navigationbar.php
@@ -1,12 +1,11 @@
 <nav>
     <ul>
         <?php
-        echo $_SERVER['HTTP_HOST'];
-        echo "<li><a href=\"" . $_SERVER['HTTP_HOST'] . "gallery/" . "\">Gallery</a></li>";
-        // echo <li><a href="../about me/">About me</a></li>
-        // echo <li><a href="../linktree/">Linktree</a></li>
-        // echo <li><a href="../contact/">Contact</a></li>
-        // echo <li><a href="../impressum/">Impressum</a></li>
+        echo "<li><a href=\"" . $_SERVER['HTTP_HOST'] . "/" . "gallery/" . "\">Gallery</a></li>";
+        echo "<li><a href=\"" . $_SERVER['HTTP_HOST'] . "/" . "about me/" . "\">About me</a></li>";
+        echo "<li><a href=\"" . $_SERVER['HTTP_HOST'] . "/" . "linktree/" . "\">Linktree</a></li>";
+        echo "<li><a href=\"" . $_SERVER['HTTP_HOST'] . "/" . "contact/" . "\">Contact</a></li>";
+        echo "<li><a href=\"" . $_SERVER['HTTP_HOST'] . "/" . "impressum/" . "\">Impressum</a></li>";
         ?>
     </ul>
 </nav>
