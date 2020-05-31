743d54581bb1235c98d43196e2aecfc66ca1cdcb
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-31 18:00:54 +0200

    Update navigationbar.js

--- heebphotography.ch/public/english/templates/navigationbar.js
+++ heebphotography.ch/public/english/templates/navigationbar.js
@@ -1,13 +1,13 @@
 function navigation_button(button) {
     button.classList.toggle("change");
-    var navigation = document.getElementById("navigation");
+    var navigation = document.getElementById("navigation-overlay");
     if (button.classList.contains("change")) {
-        alert("Going Down");
+        // alert("Going Down");
         navigation.style.height = "100%";
         document.getElementsByClassName("bar1").style.backgroundColor = rgb(255,255,255)
         document.getElementsByClassName("bar3").style.backgroundColor = rgb(255,255,255)
     } else {
-        alert("Going Up");
+        // alert("Going Up");
         navigation.style.height = "0%";
         document.getElementsByClassName("bar1").style.backgroundColor = rgb(0,0,0)
         document.getElementsByClassName("bar3").style.backgroundColor = rgb(0,0,0)
