542a662e8db4f9e0041ecd7ed2b4d9c83d27f767
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-31 18:50:07 +0200

    includes navigationbar in main english page

--- heebphotography.ch/public/english/index.php
+++ heebphotography.ch/public/english/index.php
@@ -5,29 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wildlifephotography Andreas Heeb</title>
-    <link rel="stylesheet" href="stylesheet.css">
+    <?php
+    echo "<link rel=\"stylesheet\" href=\"stylesheet.css\" href=\"https://" . $_SERVER['HTTP_HOST'] . "/templates/navigationbar.css" . ">";
+    ?>
     <script src="code.js"></script>
 </head>
 
 <body>
+    <header></header>
+    <?php include "https://" . $_SERVER['HTTP_HOST'] . "/templates/navigationbar.php"?>
     <section>
-        <header>
-            <nav>
-                <ul>
-                    <li><a href="../gallery/">Gallery</a></li>
-                    <li><a href="../about me/">About me</a></li>
-                    <li><a href="../linktree/">Linktree</a></li>
-                    <li><a href="../contact/">Contact</a></li>
-                    <li><a href="../impressum/">Impressum</a></li>
-                </ul>
-            </nav>
-        </header>
-
-        <article>
-            <header>
-                <img src="/gallery/images/example_image.jpg">
-            </header>
-        </article>
+        <article></article>
     </section>
 
     <aside>
