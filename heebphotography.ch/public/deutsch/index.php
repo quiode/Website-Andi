20e5995414fe1c04bcdf2bfacc6b8c7751a7594c
miliena <64086188+miliena@users.noreply.github.com>
2020-05-31 20:44:06 +0200

    deutsch gelöscht
    dumms gelöscht

--- heebphotography.ch/public/deutsch/index.php
+++ heebphotography.ch/public/deutsch/index.php
@@ -1,9 +1,5 @@
 <html>
     <body>
         Here's Nothing!
-        <iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="450" 
-        style="width:100%;max-width:660px;overflow:hidden;background:transparent;" 
-        sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
-        src="https://embed.music.apple.com/ch/playlist/andi-positivity/pl.u-leyl0Jlhj7P3dGg"></iframe>
     </body>
 </html>
