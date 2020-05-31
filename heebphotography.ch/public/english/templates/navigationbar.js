a4f309de1ab6dd1f18aa78541d9653c2a09375b1
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-31 17:24:04 +0200

    Update navigationbar.js

--- heebphotography.ch/public/english/templates/navigationbar.js
+++ heebphotography.ch/public/english/templates/navigationbar.js
@@ -1,6 +1,6 @@
 function navigation_button(button) {
     button.classList.toggle("change");
-    if (classList.contains("change")) {
+    if (button.classList.contains("change")) {
         alert("Going Up");
     } else {
         alert("Going Down");
