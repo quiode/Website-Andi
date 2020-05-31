45ee575e7d2815b70ff6cffb7faa2f7e37eea142
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-31 20:04:22 +0200

    Update navigationbar.php

--- heebphotography.ch/public/english/templates/navigationbar.php
+++ heebphotography.ch/public/english/templates/navigationbar.php
@@ -13,6 +13,7 @@
     <div id="navigation-overlay">
         <div id="navigation-content">
             <?php
+        echo "<a class=\"links\" href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "" . "\">Home</a>";
         echo "<a class=\"links\" href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "gallery/" . "\">Gallery</a>";
         echo "<a class=\"links\" href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "about me/" . "\">About me</a>";
         echo "<a class=\"links\" href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "linktree/" . "\">Linktree</a>";
