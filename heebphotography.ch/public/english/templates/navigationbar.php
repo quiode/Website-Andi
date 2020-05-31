84cc1436b4e4dea5e9538a475a23e2ef13402b7d
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-31 15:55:26 +0200

    Update navigationbar.php

--- heebphotography.ch/public/english/templates/navigationbar.php
+++ heebphotography.ch/public/english/templates/navigationbar.php
@@ -1,12 +1,12 @@
 <nav>
     <ul>
         <?php
-        echo $_SERVER["hostname"];
+        echo $_SERVER['HTTP_HOST'];
+        echo "<li><a href=\"" . $_SERVER['HTTP_HOST'] . "gallery/" . "\">Gallery</a></li>";
+        // echo <li><a href="../about me/">About me</a></li>
+        // echo <li><a href="../linktree/">Linktree</a></li>
+        // echo <li><a href="../contact/">Contact</a></li>
+        // echo <li><a href="../impressum/">Impressum</a></li>
         ?>
-        <li><a href="../gallery/">Gallery</a></li>
-        <li><a href="../about me/">About me</a></li>
-        <li><a href="../linktree/">Linktree</a></li>
-        <li><a href="../contact/">Contact</a></li>
-        <li><a href="../impressum/">Impressum</a></li>
     </ul>
 </nav>
