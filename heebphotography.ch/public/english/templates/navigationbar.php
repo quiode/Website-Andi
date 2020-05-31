ef4725bf397dc537a4b4f506889f91c353d760be
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-31 16:38:26 +0200

    javascript

--- heebphotography.ch/public/english/templates/navigationbar.php
+++ heebphotography.ch/public/english/templates/navigationbar.php
@@ -1,4 +1,12 @@
 <nav>
+    <?php
+    echo "<script src=\"" . "https://" . $_SERVER['HTTP_HOST'] . "/templates/navigationbar.js" . "\"></script>"
+    ?>
+    <div id="navigation_button" onclick="navigation_button(this)">
+        <div id="bar1"></div>
+        <div id="bar2"></div>
+        <div id="bar3"></div>
+    </div>
     <ul>
         <?php
         echo "<li><a href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "gallery/" . "\">Gallery</a></li>";
