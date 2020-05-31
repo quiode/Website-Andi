0658de65fb9248ee6f60f1ac679c93b76c01bdf0
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-31 16:02:41 +0200

    Update navigationbar.php

--- heebphotography.ch/public/english/templates/navigationbar.php
+++ heebphotography.ch/public/english/templates/navigationbar.php
@@ -1,11 +1,11 @@
 <nav>
     <ul>
         <?php
-        echo "<li><a href=\"" . $_SERVER['HTTP_HOST'] . "/" . "gallery/" . "\">Gallery</a></li>";
-        echo "<li><a href=\"" . $_SERVER['HTTP_HOST'] . "/" . "about me/" . "\">About me</a></li>";
-        echo "<li><a href=\"" . $_SERVER['HTTP_HOST'] . "/" . "linktree/" . "\">Linktree</a></li>";
-        echo "<li><a href=\"" . $_SERVER['HTTP_HOST'] . "/" . "contact/" . "\">Contact</a></li>";
-        echo "<li><a href=\"" . $_SERVER['HTTP_HOST'] . "/" . "impressum/" . "\">Impressum</a></li>";
+        echo "<li><a href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "gallery/" . "\">Gallery</a></li>";
+        echo "<li><a href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "about me/" . "\">About me</a></li>";
+        echo "<li><a href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "linktree/" . "\">Linktree</a></li>";
+        echo "<li><a href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "contact/" . "\">Contact</a></li>";
+        echo "<li><a href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "impressum/" . "\">Impressum</a></li>";
         ?>
     </ul>
 </nav>
