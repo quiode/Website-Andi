885e14719a51d7bab3fbf134fbf855c0d3f4b09f
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-31 19:34:11 +0200

    Update index.php

--- heebphotography.ch/public/english/index.php
+++ heebphotography.ch/public/english/index.php
@@ -5,19 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wildlifephotography Andreas Heeb</title>
-    <?php
-    echo "<link rel=\"stylesheet\" href=\"stylesheet.css\" href=\"" . $_SERVER['DOCUMENT_ROOT'] . "/templates/navigationbar.css" . ">";
-    ?>
+    <link rel="stylesheet" href="stylesheet.css" href="./templates/navigationbar.css">
     <script src="code.js"></script>
 </head>
 
 <body>
     <header></header>
-    <?php 
-    $navigationbar_file = "https://" . $_SERVER['HTTP_HOST'] . "/templates/navigationbar.php";
-    echo $navigationbar_file;
-    include $navigationbar_file;
-    ?>
+    <?php include './templates/navigationbar.php'?>
     <section>
         <article></article>
     </section>
