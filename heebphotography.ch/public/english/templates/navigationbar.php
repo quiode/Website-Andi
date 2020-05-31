0a8db3fb6122fd2adec63b6fd7fecde261df217a
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-05-31 15:49:46 +0200

    navigationbar

---
+++ heebphotography.ch/public/english/templates/navigationbar.php
@@ -0,0 +1,12 @@
+<nav>
+    <ul>
+        <?php
+        echo $_SERVER["hostname"];
+        ?>
+        <li><a href="../gallery/">Gallery</a></li>
+        <li><a href="../about me/">About me</a></li>
+        <li><a href="../linktree/">Linktree</a></li>
+        <li><a href="../contact/">Contact</a></li>
+        <li><a href="../impressum/">Impressum</a></li>
+    </ul>
+</nav>
