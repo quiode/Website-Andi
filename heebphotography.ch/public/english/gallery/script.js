59fb08e86fb6f0d08b3365292298b00e59b90004
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-06-17 00:10:48 +0200

    Update script.js

--- heebphotography.ch/public/english/gallery/script.js
+++ heebphotography.ch/public/english/gallery/script.js
@@ -1,6 +1,6 @@
 //  resizes the image depending on its height/width and the width/height of the window
 function resizeToMax() {
-    myImage = new Image()
+    myImage = new Image();
     var img = document.getElementById("slideshow_image");
     if (img.src.width / document.body.clientWidth > img.src.height / document.body.clientHeight) {
         img.style.width = "100%";
