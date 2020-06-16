b4198ee854773d59273a0a3d4b29b3622e5ab7c3
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-06-17 00:21:54 +0200

    Update navigationbar.js

--- heebphotography.ch/public/english/templates/navigationbar.js
+++ heebphotography.ch/public/english/templates/navigationbar.js
@@ -7,15 +7,17 @@ function navigation_button(button) {
         // starts transformation --> element goes down
         navigation.style.height = "100%";
         // makes the cross black when the background gets whiter
-        document.getElementById("b1").style.backgroundColor = "rgb(255,255,255)"
-        document.getElementById("b2").style.backgroundColor = "rgb(255,255,255)"
-        document.getElementById("b3").style.backgroundColor = "rgb(255,255,255)"
+        document.getElementById("b1").style.backgroundColor = "rgb(255,255,255)";
+        document.getElementById("b2").style.backgroundColor = "rgb(255,255,255)";
+        document.getElementById("b3").style.backgroundColor = "rgb(255,255,255)";
+        document.getElementsByTagName("body")[0].style.overflow = "hidden";
     } else {
         // starts transformation --> elements goes up
         navigation.style.height = "0%";
         // makes the cross white when the background gets darker
-        document.getElementById("b1").style.backgroundColor = "rgb(0,0,0)"
-        document.getElementById("b2").style.backgroundColor = "rgb(0,0,0)"
-        document.getElementById("b3").style.backgroundColor = "rgb(0,0,0)"
+        document.getElementById("b1").style.backgroundColor = "rgb(0,0,0)";
+        document.getElementById("b2").style.backgroundColor = "rgb(0,0,0)";
+        document.getElementById("b3").style.backgroundColor = "rgb(0,0,0)";
+        document.getElementsByTagName("body")[0].style.overflow = "auto";
     }
-  } 
+}
