c591ced1b11947b57f7b797a8d04e280a582df76
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-06-16 18:14:35 +0200

    slideshow wip

--- heebphotography.ch/public/english/templates/navigationbar.php
+++ heebphotography.ch/public/english/templates/navigationbar.php
@@ -4,7 +4,7 @@
     echo "<script src=\"" . "https://" . $_SERVER['HTTP_HOST'] . "/templates/navigationbar.js" . "\"></script>"
     ?>
     <!-- makes the button for navigation -->
-    <div class="navigation_button" onclick="navigation_button(this)">
+    <div id="navigation_button" onclick="navigation_button(this)">
         <div class="bar1" id="b1"></div>
         <div class="bar2" id="b2"></div>
         <div class="bar3" id="b3"></div>
