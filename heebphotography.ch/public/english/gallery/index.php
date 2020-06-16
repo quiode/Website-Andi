0d25b06bac6151cf02ad0d67db8afefe2ce553d1
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-06-16 23:15:21 +0200

    php zeugs

--- heebphotography.ch/public/english/gallery/index.php
+++ heebphotography.ch/public/english/gallery/index.php
@@ -20,7 +20,7 @@
     <div id="gallery">
         <?php
         // searches the filename of all images in the images folder
-        $images =  scandir("images", 1);
+        $images = scandir("images", 1);
         // deletes the last two elements of the filename list --> are always . and .. so not usefull
         unset($images[sizeof($images)-1]);
         unset($images[sizeof($images)-1]);
@@ -28,7 +28,7 @@
         $image_column_1 = array();
         $image_column_2 = array();
         $image_column_3 = array();
-        $image_column_4 = array();  
+        $image_column_4 = array();
         $i = 0;
         $c = 0;
         while ($i <= sizeof($images)) {
