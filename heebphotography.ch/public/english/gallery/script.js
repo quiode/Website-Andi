255cd0bb20e503b725a36fd6d54fdf5fddfbc2f0
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-06-16 15:27:27 +0200

    cleaned code, new testing folder

--- heebphotography.ch/public/english/gallery/script.js
+++ heebphotography.ch/public/english/gallery/script.js
@@ -1,8 +1,8 @@
 //  resizes the image depending on its height/width and the width/height of the window
-function resizeToMax(){
-    myImage = new Image() 
+function resizeToMax() {
+    myImage = new Image()
     var img = document.getElementById("slideshow_image");
-    if(img.src.width / document.body.clientWidth > img.src.height / document.body.clientHeight){
+    if (img.src.width / document.body.clientWidth > img.src.height / document.body.clientHeight) {
         img.style.width = "100%";
     } else {
         img.style.height = "100%";
