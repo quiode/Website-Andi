71a0228aba7050e561d09a913ce64a0a1805dd7b
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-07-16 21:35:06 +0200

    tesing htaccess

--- heebphotography.ch/private/test.php
+++ heebphotography.ch/private/test.php
@@ -2,10 +2,7 @@
     This is just for texting
     <?php
     $myfile = fopen("testfile.txt", "a+");
-    $txt = "John Doe\n";
-    fwrite($myfile, $txt);
-    $txt = "Jane Doe\n";
-    fwrite($myfile, $txt);
+    fwrite($myfile, $_POST["message"]);
     fclose($myfile);
     ?>
 </p>
