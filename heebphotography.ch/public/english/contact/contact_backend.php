7657b00d28fbf53352061ab4b5df4f2738e20ca4
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-07-16 22:56:53 +0200

    email :/

--- heebphotography.ch/public/english/contact/contact_backend.php
+++ heebphotography.ch/public/english/contact/contact_backend.php
@@ -32,38 +32,11 @@
         //     echo "ERROR!2";
         // }
 
-        
-        $to = "domi.schwaiger04@gmail.com, admin@heebphotography.ch";
-        $subject = "HTML email";
-
-        $message = "
-        <html>
-        <head>
-        <title>HTML email</title>
-        </head>
-        <body>
-        <p>This email contains HTML Tags!</p>
-        <table>
-        <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        </tr>
-        <tr>
-        <td>John</td>
-        <td>Doe</td>
-        </tr>
-        </table>
-        </body>
-        </html>
-        ";
-
-        // Always set content-type when sending HTML email
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-        // More headers
-        $headers .= 'From: <webmaster@example.com>' . "\r\n";
-        $headers .= 'Cc: myboss@example.com' . "\r\n";
+        $to = "domi.schwaiger04@gmail.com";
+        $subject = "My subject";
+        $txt = "Hello world!";
+        $headers = "From: webmaster@example.com" . "\r\n" .
+        "CC: somebodyelse@example.com";
 
         if (mail($to, $subject, $message, $headers)) {
             echo "yey";
