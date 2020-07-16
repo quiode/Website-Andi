e85043df2ba53e1aacfa83a710ebc58c5f9d3b28
miliena <64086188+miliena@users.noreply.github.com>
2020-07-16 16:08:17 +0200

    js code tauschen
    ^

--- heebphotography.ch/public/english/templates/navigationbar.js
+++ heebphotography.ch/public/english/templates/navigationbar.js
@@ -8,19 +8,19 @@ function navigation_button(button) {
         navigation.style.height = "100%";
         // makes the cross black when the background gets whiter
         document.getElementById("b1").style.backgroundColor = "rgb(255,255,255)";
-        document.getElementById("b1").style.border = "1px white solid";
-        document.getElementById("b1").style.width = "70px";
-        document.getElementById("b1").style.height = "10px";
+        document.getElementById("b1").style.border = "none";
+        document.getElementById("b1").style.width = "72px";
+        document.getElementById("b1").style.height = "12px";
 
         document.getElementById("b2").style.backgroundColor = "rgb(255,255,255)";
-        document.getElementById("b2").style.border = "1px white solid";
-        document.getElementById("b2").style.width = "70px";
-        document.getElementById("b2").style.height = "10px";
+        document.getElementById("b2").style.border = "none";
+        document.getElementById("b2").style.width = "72px";
+        document.getElementById("b2").style.height = "12px";
 
         document.getElementById("b3").style.backgroundColor = "rgb(255,255,255)";
-        document.getElementById("b3").style.border = "1px white solid";
-        document.getElementById("b3").style.width = "70px";
-        document.getElementById("b3").style.height = "10px";
+        document.getElementById("b3").style.border = "none";
+        document.getElementById("b3").style.width = "72px";
+        document.getElementById("b3").style.height = "12px";
 
         document.getElementsByTagName("body")[0].style.overflow = "hidden";
     } else {
@@ -28,19 +28,19 @@ function navigation_button(button) {
         navigation.style.height = "0%";
         // makes the cross white when the background gets darker
         document.getElementById("b1").style.backgroundColor = "rgb(0,0,0)";
-        document.getElementById("b1").style.border = "none";
-        document.getElementById("b1").style.width = "72px";
-        document.getElementById("b1").style.height = "12px";
+        document.getElementById("b1").style.border = "1px #fff solid";
+        document.getElementById("b1").style.width = "70px";
+        document.getElementById("b1").style.height = "10px";
         
         document.getElementById("b2").style.backgroundColor = "rgb(0,0,0)";
-        document.getElementById("b2").style.border = "none";
-        document.getElementById("b2").style.width = "72px";
-        document.getElementById("b2").style.height = "12px";
+        document.getElementById("b2").style.border = "1px #fff solid";
+        document.getElementById("b2").style.width = "70px";
+        document.getElementById("b2").style.height = "10px";
         
         document.getElementById("b3").style.backgroundColor = "rgb(0,0,0)";
-        document.getElementById("b3").style.border = "none";
-        document.getElementById("b3").style.width = "72px";
-        document.getElementById("b3").style.height = "12px";
+        document.getElementById("b3").style.border = "1px #fff solid";
+        document.getElementById("b3").style.width = "70px";
+        document.getElementById("b3").style.height = "10px";
 
         document.getElementsByTagName("body")[0].style.overflow = "auto";
     }
