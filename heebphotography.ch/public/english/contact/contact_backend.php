058496e16f76a2359cb3c72712b98a3e34f02de3
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-07-17 20:04:39 +0200

    php mail

--- heebphotography.ch/public/english/contact/contact_backend.php
+++ heebphotography.ch/public/english/contact/contact_backend.php
@@ -7,7 +7,7 @@
 
 <body>
     <?php
-    print_r($_POST);
+    // print_r($_POST);
     // makes the message which is send per mail
     $subject .= ($_POST["first_name"] != "") ? $_POST["first_name"] : "NoFirstNameGiven";
     $subject .= " ";
@@ -16,21 +16,38 @@
     $subject .= ($_POST["subject"] != "custom") ? $_POST["subject"] : $_POST["custom_subject"];
     $subject .= ". ";
     $subject .= ($_POST["how_to_contact"] != "anonymous") ? ("You can reach him via " . $_POST["how_to_contact"] . " under " . $_POST[$_POST["how_to_contact"]] . ".") : "No information on how to contact is given.";
-    echo "<br>";
-    echo $subject;
-    $header = ($_POST["how_to_contact"] = "email") ? "From: <" . $_POST[$_POST["how_to_contact"]] . ">" : "From: <admin@heebphotography.ch>";
-    echo "<br>";
+    // echo "<br>";
+    // echo $subject;
+    // the header (sender, return, cc, just more information)
+    // makes the mail which the messages was sent from depending on the user input
+    $mail = ($_POST["first_name"] != "") ? $_POST["first_name"] : "NoFirstNameGiven";
+    $mail .= " ";
+    $mail .= ($_POST["last_name"] != "") ? $_POST["last_name"] : "NoLastNameGiven";
+    $mail .= " <";
+    $mail .= $_POST["mail"];
+    $mail .= ">";
+    $mail = ($_POST["mail"] != "") ? $mail : "Contact Form <admin@heebphotography.ch>";
+
+    $headers .= "Reply-To: " . $mail . "\r\n";
+    $headers .= "Return-Path: " . $mail . "\r\n";
+    $header = "From: " . $mail;
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/plain;charset=UTF-8" . "\r\n";
+    $headers .= "X-Mailer: PHP". phpversion() ."\r\n";
+
+    // echo "<br>";
+    // sends mail and sais if it succeeded or failed
     if (mail("andreas.heeb@heebphotography.ch", $subject, $_POST["message"], $header)) {
         echo "SUCCESS!";
     } else {
         echo "ERROR!";
     }
-        echo "<br>";
-    if (mail("domi.schwaiger04@gmail.com", "test", "test", "From: <domi.schwaiger04@gmail.com>")) {
-        echo "SUCCESS!2";
-    } else {
-        echo "ERROR!2";
-    }
+    //     echo "<br>";
+    // if (mail("domi.schwaiger04@gmail.com", "test", "test", "From: <domi.schwaiger04@gmail.com>")) {
+    //     echo "SUCCESS!2";
+    // } else {
+    //     echo "ERROR!2";
+    // }
 
         // $to = "domi.schwaiger04@gmail.com";
         // $subject = "My subject";
