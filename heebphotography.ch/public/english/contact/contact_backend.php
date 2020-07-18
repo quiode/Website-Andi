b4a0bb14f10b05be2c7ade77df905493b1377b3b
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-07-18 10:52:27 +0200

    testing mail

--- heebphotography.ch/public/english/contact/contact_backend.php
+++ heebphotography.ch/public/english/contact/contact_backend.php
@@ -17,20 +17,21 @@
     $subject .= ($_POST["how_to_contact"] != "anonymous") ? ("You can reach him via " . $_POST["how_to_contact"] . " under " . $_POST[$_POST["how_to_contact"]] . ".") : "No information on how to contact is given.";
     // the header (sender, return, cc, just more information)
     // makes the mail which the messages was sent from depending on the user input
-    $mail = ($_POST["first_name"] != "") ? $_POST["first_name"] : "NoFirstNameGiven";
-    $mail .= " ";
-    $mail .= ($_POST["last_name"] != "") ? $_POST["last_name"] : "NoLastNameGiven";
-    $mail .= " <";
-    $mail .= $_POST["mail"];
-    $mail .= ">";
-    $mail = ($_POST["mail"] != "") ? $mail : "Contact Form <admin@heebphotography.ch>";
+    // $mail = ($_POST["first_name"] != "") ? $_POST["first_name"] : "NoFirstNameGiven";
+    // $mail .= " ";
+    // $mail .= ($_POST["last_name"] != "") ? $_POST["last_name"] : "NoLastNameGiven";
+    // $mail .= " <";
+    // $mail .= $_POST["mail"];
+    // $mail .= ">";
+    // $mail = ($_POST["mail"] != "") ? $mail : "Contact Form <admin@heebphotography.ch>";
+    $mail = "Dominik Schwaiger <dominik.schwaiger@heebphotography.ch>";
     // the headers
-    $headers = "Reply-To: " . $mail . "\r\n";
-    $headers .= "Return-Path: " . $mail . "\r\n";
-    $headers .= "From: " . $mail;
-    $headers .= "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/plain;charset=UTF-8" . "\r\n";
-    $headers .= "X-Mailer: PHP". phpversion() ."\r\n";
+    $header = "Reply-To: " . $mail . "\r\n";
+    $header .= "Return-Path: " . $mail . "\r\n";
+    $header .= "From: " . $mail;
+    $header .= "MIME-Version: 1.0" . "\r\n";
+    $header .= "Content-type:text/plain;charset=UTF-8" . "\r\n";
+    $header .= "X-Mailer: PHP". phpversion() ."\r\n";
     // sends mail and sais if it succeeded or failed
     if (mail("andreas.heeb@heebphotography.ch", $subject, $_POST["message"], $header)) {
         echo "SUCCESS!";
