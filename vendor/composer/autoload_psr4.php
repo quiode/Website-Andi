3ed80419c4e9fcff0efb48c65685d814b6919c24
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-07-18 16:26:40 +0200

    removed mdi library

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,5 +7,4 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
-    'Mdi\\' => array($vendorDir . '/mesavolt/mdi-php/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,20 +11,12 @@ class ComposerStaticInit22c13e5ae88762810c141fe0a39319f2
         array (
             'PHPMailer\\PHPMailer\\' => 20,
         ),
-        'M' => 
-        array (
-            'Mdi\\' => 4,
-        ),
     );
 
     public static $prefixDirsPsr4 = array (
         'PHPMailer\\PHPMailer\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
-        ),
-        'Mdi\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/mesavolt/mdi-php/src',
         ),
     );
 
