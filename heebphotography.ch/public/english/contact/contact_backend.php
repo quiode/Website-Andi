7237cc2a2db4ef01fd963ea30a48bc32c4a81c2b
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-07-18 10:49:59 +0200

    back to normal

--- heebphotography.ch/public/english/contact/contact_backend.php
+++ heebphotography.ch/public/english/contact/contact_backend.php
@@ -17,14 +17,13 @@
     $subject .= ($_POST["how_to_contact"] != "anonymous") ? ("You can reach him via " . $_POST["how_to_contact"] . " under " . $_POST[$_POST["how_to_contact"]] . ".") : "No information on how to contact is given.";
     // the header (sender, return, cc, just more information)
     // makes the mail which the messages was sent from depending on the user input
-    // $mail = ($_POST["first_name"] != "") ? $_POST["first_name"] : "NoFirstNameGiven";
-    // $mail .= " ";
-    // $mail .= ($_POST["last_name"] != "") ? $_POST["last_name"] : "NoLastNameGiven";
-    // $mail .= " <";
-    // $mail .= $_POST["mail"];
-    // $mail .= ">";
-    // $mail = ($_POST["mail"] != "") ? $mail : "Contact Form <admin@heebphotography.ch>";
-    $mail = "Dominik Schwaiger <dominik.schwaiger@heebphotography.ch>";
+    $mail = ($_POST["first_name"] != "") ? $_POST["first_name"] : "NoFirstNameGiven";
+    $mail .= " ";
+    $mail .= ($_POST["last_name"] != "") ? $_POST["last_name"] : "NoLastNameGiven";
+    $mail .= " <";
+    $mail .= $_POST["mail"];
+    $mail .= ">";
+    $mail = ($_POST["mail"] != "") ? $mail : "Contact Form <admin@heebphotography.ch>";
     // the headers
     $headers = "Reply-To: " . $mail . "\r\n";
     $headers .= "Return-Path: " . $mail . "\r\n";
