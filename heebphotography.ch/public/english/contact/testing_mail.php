292a6f775f1d8548bf87c9260a87cd8afc7964e9
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-07-18 18:48:59 +0200

    trying once again with just a very simple local mail

--- heebphotography.ch/public/english/contact/testing_mail.php
+++ heebphotography.ch/public/english/contact/testing_mail.php
@@ -22,26 +22,20 @@
     require "./PHPMailer-master/src/" . "SMTP.php";
 //PHPMailer Object
     $mail = new PHPMailer(true); //Argument true in constructor enables exceptions
-// SMTP Server
-    $mail->SMTPAuth   = true;                  // enable SMTP authentication
-    $mail->Host       = "smtp.gmail.com"; // sets the SMTP server
-    $mail->Port       = 587;                    // sets the SMTP port
-    $mail->SMTPSecure = 'tls';                  // TLS/SSL
-    $mail->Username   = "contact.form.heebphotography@gmail.com"; // SMTP account username
-    $mail->Password   = "Yw63Bf#@uK@t%P7";        // SMTP account password
 //From email address and name
-    $mail->From = "contact.form.heebphotography@gmail.com";
-    // $mail->FromName = "Contact Form";
+    $mail->From = "admin@heebphotography.ch";
+    $mail->FromName = "Contact Form";
 //To address and name
-    $mail->addAddress("domi.schwaiger04@gmail.com"); //Recipient
+    $mail->addAddress("contact.form.heebphotography@gmail.com");
 //Address to which recipient will reply
-    $mail->addReplyTo("contact.form.heebphotography@gmail.com", "Reply");
+    $mail->addReplyTo("admin@heebphotography.ch", "Reply");
 //Send HTML or Plain Text email
     $mail->isHTML(true);
-    $mail->Subject = "Testing PHP Mailer";
+
+    $mail->Subject = "Subject Text";
     $mail->Body = "<i>Mail body in HTML</i>";
     $mail->AltBody = "This is the plain text version of the email content";
-// sends the actual email
+
     try {
         $mail->send();
         echo "Message has been sent successfully";
