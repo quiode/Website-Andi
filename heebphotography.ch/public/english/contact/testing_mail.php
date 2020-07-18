bcc547d5ff9785daa2016c23d349ee505b592297
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-07-18 17:11:10 +0200

    removed testing code

--- heebphotography.ch/public/english/contact/testing_mail.php
+++ heebphotography.ch/public/english/contact/testing_mail.php
@@ -7,14 +7,8 @@
 
 <body>
     <?php
-// for testing
-    // echo php_ini_loaded_file();
 // imports all components
-    // echo "1"; //for testing
-
     set_include_path("PHPMailer-master");
-
-    // echo "2"; //for testing
 
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
@@ -22,8 +16,6 @@
     require $_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "PHPMailer-master" . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "Exception.php";
     require $_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "PHPMailer-master" . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "PHPMailer.php";
     require $_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "PHPMailer-master" . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "SMTP.php";
-
-    // echo "3"; //for testing
 
 //PHPMailer Object
     $mail = new PHPMailer(true); //Argument true in constructor enables exceptions
@@ -39,10 +31,6 @@
 //Address to which recipient will reply
     $mail->addReplyTo("admin@heebphotography.ch", "Reply");
 
-//CC and BCC
-    // $mail->addCC("cc@example.com");
-    // $mail->addBCC("bcc@example.com");
-
 //Send HTML or Plain Text email
     $mail->isHTML(true);
 
@@ -56,8 +44,6 @@
     } catch (Exception $e) {
         echo "Mailer Error: " . $mail->ErrorInfo;
     }
-
-    // echo "4"; //for testing
     ?>
 </body>
 
