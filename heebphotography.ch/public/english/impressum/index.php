ba4ffc2617a9307d29479bf162e3521a3d3e0ebf
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-07-19 22:19:59 +0200

    about_me.css to scss

--- heebphotography.ch/public/english/impressum/index.php
+++ heebphotography.ch/public/english/impressum/index.php
@@ -5,11 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Impressum | Wildlifephotography Andreas Heeb</title>
-    <link rel="stylesheet" href="stylesheet.css">
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap" rel="stylesheet">
-    <?php
+    <link rel="stylesheet" href="https://heebphotography.ch/public/styles/main.css">
+    <!-- <?php
     echo "<link rel=\"stylesheet\" href=\"https://" . $_SERVER['HTTP_HOST'] . "/templates/navigationbar.css" . "\">";
-    ?>
+    ?> -->
 </head>
 
 <body>
