09fcb94ad2a040d33f1fd512c1a7e230f960b7c4
miliena <64086188+miliena@users.noreply.github.com>
2020-08-28 14:15:54 +0200

    JS bearbeitet
    angepasst und mit kommentaren ergänzt

--- heebphotography.ch/public/english/script.js
+++ heebphotography.ch/public/english/script.js
@@ -1,16 +1,27 @@
 function slideshow() {
-    var slideIndex = 0;
-    showSlides();
+  var slideIndex = 1;
+  showSlides();
 }
 
 function showSlides() {
-  var i;
+
   var slides = document.getElementsByClassName("slide");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  //speichert alle slides in einem array    
+  
+  if (slideIndex > slides.length) { 
+    slideIndex = 1;  
+    //um Index zu resetten wenn er grösser als die Anzahl Bilder wird                                      
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    //macht alle slides unsichtbar   
+    slideIndex++;                    
+  }
+
+
+  slides[slideIndex - 1].style.display = "block";
+  //macht eine Slide sichtbar
+
   setTimeout(showSlides, 2000);
 }
