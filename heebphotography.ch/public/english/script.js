481dedeff3540f9293a65f975706c50b17c658d5
miliena <64086188+miliena@users.noreply.github.com>
2020-08-28 14:42:43 +0200

    js verändert
    ka wasi mach bin chli lost

--- heebphotography.ch/public/english/script.js
+++ heebphotography.ch/public/english/script.js
@@ -8,20 +8,18 @@ function showSlides() {
   var slides = document.getElementsByClassName("slide");
   //speichert alle slides in einem array    
   
-  if (slideIndex > slides.length) { 
-    slideIndex = 1;  
-    //um Index zu resetten wenn er grösser als die Anzahl Bilder wird                                      
-  }
-  
   for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-    //macht alle slides unsichtbar                       
+    //macht alle slides unsichtbar
+    slideIndex++;    
+    
+    if (slideIndex > slides.length) { 
+      slideIndex = 1;  
+      //um Index zu resetten wenn er grösser als die Anzahl Bilder wird                                      
+    }      
+    
+    slides[slideIndex - 1].style.display = "block";
+    //macht eine Slide sichtbar
+    setTimeout(showSlides, 2000);
   }
-
-  slideIndex++;
-
-  slides[slideIndex - 1].style.display = "block";
-  //macht eine Slide sichtbar
-
-  setTimeout(showSlides, 2000);
 }
