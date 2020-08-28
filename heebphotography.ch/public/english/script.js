e5cfe0129485ec59a274b2ff937d1887bcf91735
miliena <64086188+miliena@users.noreply.github.com>
2020-08-28 14:50:34 +0200

    js zu kommentar gemacht

--- heebphotography.ch/public/english/script.js
+++ heebphotography.ch/public/english/script.js
@@ -1,4 +1,4 @@
-document.onload = function slideshow() {
+/* document.onload = function slideshow() {
   showSlides();
 }
 
@@ -23,3 +23,4 @@ function showSlides() {
     setTimeout(showSlides, 10000);
   }
 }
+*/
