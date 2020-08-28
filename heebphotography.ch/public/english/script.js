a88e9983bd6935c4340bf411ca84c465aa43f1eb
miliena <64086188+miliena@users.noreply.github.com>
2020-08-28 15:22:07 +0200

    ka was ic hno uusprobiere söll

--- heebphotography.ch/public/english/script.js
+++ heebphotography.ch/public/english/script.js
@@ -36,7 +36,7 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
