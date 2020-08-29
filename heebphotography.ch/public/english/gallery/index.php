ba332288a0caf6aa223cd28eb0c559bf4d3d286b
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-08-29 16:30:08 +0200

    trying to fix a bug

--- heebphotography.ch/public/english/gallery/index.php
+++ heebphotography.ch/public/english/gallery/index.php
@@ -24,6 +24,8 @@
         // searches the filename of all images in the images folder
         $images = scandir($path_images, 1);
         print_r($images);
+        echo $images;
+        echo $images;
         print_r("lol help");
         // deletes the last two elements of the filename list --> are always . and .. so not usefull
         unset($images[sizeof($images)-1]);
