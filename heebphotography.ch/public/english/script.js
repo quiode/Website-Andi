248cff568c7518b5ae609eebc559c83218a0d4e3
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-01 00:16:08 +0200

    debugging

--- heebphotography.ch/public/english/script.js
+++ heebphotography.ch/public/english/script.js
@@ -6,6 +6,7 @@ function slideshow() {
 	alert("help");
 	// when the document has fully loaded, begin with slideshow
 	if (document.readyState == "complete") {
+		alert("1");
 		// gets every slide image
 		var slides = document.getElementsByClassName("slides");
 		// finds the slide image which is currently shown
@@ -14,11 +15,13 @@ function slideshow() {
 				current_slide = slides[index];
 			}
 		}
+		alert("2");
 		// select a new slide image
 		var new_slide = current_slide;
 		while (new_slide == current_slide) {
 			new_slide = slides[Math.floor(Math.random() * slides.length)];
 		}
+		alert("3");
 		// lets the current slide disappear and shows the new slide slide
 		current_slide.style.opasity = 0;
 		new_slide.style.opasity = 1;
