6dd69aa3e8fb30a62f80c4c1ea48abe227901c00
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-01 00:33:27 +0200

    i think i fixed it (slideshow)

--- heebphotography.ch/public/english/script.js
+++ heebphotography.ch/public/english/script.js
@@ -1,9 +1,8 @@
 // calls the slideshow fuction, which changes the image, every 5 seconds
-var slideshow_interval = setInterval(slideshow(), 500);
+var slideshow_interval = setInterval(slideshow, 5000);
 
 // changes the image
 function slideshow() {
-	alert("lol");
 	// when the document has fully loaded, begin with slideshow
 	if (document.readyState == "complete") {
 		// gets every slide image
