ea00cdd20560b57753d8411192018747c8dfe911
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-08-31 23:50:57 +0200

    changed js completely to a interval thing and made the first im visible

--- heebphotography.ch/public/english/script.js
+++ heebphotography.ch/public/english/script.js
@@ -1,25 +1,14 @@
-function slideshow(sliding_time) {
-	// gets the slides
-	var slides = document.getElementsByClassName("slide");
-	// makes a list of all the src of the images
-	var all_img_src = [];
-	alert(slides.length);
-	alert(slides[0]);
-	alert(slides[0].src);
-	for (let i = 0; i < slides.length; i++) {
-		all_img_src.push(slides[i].src);
-		alert(slides[i].src);
-	}
+// calls the slideshow fuction, which changes the image, every 5 seconds
+setInterval(slideshow(), 5000);
 
-	// the slideshow
-	slides.forEach(slide => {
-		setTimeout(() => {
-			slide.style.opacity = 1;
-			if (all_img_src.indexOf(slide.src) == 0) {
-				slides[all_img_src.length - 1].style.opacity = 0;
-			} else {
-				slides[all_img_src.indexOf(slide.src) - 1].style.opacity = 0;
-			}
-		}, sliding_time);
-	});
+// changes the image
+function slideshow() {
+	if (document.readyState == "complete"){
+		// gets every slide image
+		var slides = document.getElementsByClassName("slides");
+	
+		// finds the slide image which is currently shown
+	
+		// changes the slide image to a random one
+	}
 }
