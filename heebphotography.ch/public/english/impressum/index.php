d8ba9aebc5fc0d87ce77c766174b0adb8ab98193
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-02 18:37:32 +0200

    testing smth

--- heebphotography.ch/public/english/impressum/index.php
+++ heebphotography.ch/public/english/impressum/index.php
@@ -20,7 +20,7 @@
                 <p><strong>Haftungsausschluss</strong></p>Der Autor &uuml;bernimmt keinerlei Gew&auml;hr hinsichtlich
                 der inhaltlichen Richtigkeit, Genauigkeit, Aktualit&auml;t, Zuverl&auml;ssigkeit und
                 Vollst&auml;ndigkeit der Informationen.</p>
-                <p>Haftungsanspr&uuml;che gegen den Autor wegen Sch&auml;den materieller oder immaterieller Art, welche
+                <p id="test1">Haftungsanspr&uuml;che gegen den Autor wegen Sch&auml;den materieller oder immaterieller Art, welche
                     aus dem Zugriff oder der Nutzung bzw. Nichtnutzung der ver&ouml;ffentlichten Informationen, durch
                     Missbrauch der Verbindung oder durch technische St&ouml;rungen entstanden sind, werden
                     ausgeschlossen.</p>
@@ -32,7 +32,7 @@
                 <p>Verweise und Links auf Webseiten Dritter liegen ausserhalb unseres Verantwortungsbereichs. Es wird
                     jegliche Verantwortung f&uuml;r solche Webseiten abgelehnt. Der Zugriff und die Nutzung solcher
                     Webseiten erfolgen auf eigene Gefahr des jeweiligen Nutzers.</p>
-                <p>&nbsp;</p>
+                <p id="test2">&nbsp;</p>
                 <p><strong>Urheberrechte</strong></p>
                 <p>Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf dieser
                     Website, geh&ouml;ren ausschliesslich <strong>Dominik Schwaiger</strong> oder den speziell genannten
