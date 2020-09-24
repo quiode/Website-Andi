64e51557aa78e72c6e2fda2db3ad233adb10f628
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-24 11:57:51 +0200

    auto renaming images

--- heebphotography.ch/public/english/gallery/images/renaming.py
+++ heebphotography.ch/public/english/gallery/images/renaming.py
@@ -5,3 +5,5 @@ from os import rename
 images_dir = input("Path to images:")
 
 all_images = [f for f in listdir(images_dir) if isfile(join(images_dir, f))]
+for counter, image in enumerate(all_images):
+    rename(images_dir + image, images_dir + "image_" + str(counter) + ".jpg")
