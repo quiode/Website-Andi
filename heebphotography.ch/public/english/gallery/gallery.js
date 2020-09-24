2128366bdbc8562bf5c7fee07f4314ef034581b8
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-24 15:16:36 +0200

    Update gallery.js

--- heebphotography.ch/public/english/gallery/gallery.js
+++ heebphotography.ch/public/english/gallery/gallery.js
@@ -97,9 +97,9 @@ function swipedetect(callback) {
         startY,
         distX,
         distY,
-        threshold = 150, //required min distance traveled to be considered swipe
+        threshold = 100, //required min distance traveled to be considered swipe
         restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-        allowedTime = 300, // maximum time allowed to travel that distance
+        allowedTime = 400, // maximum time allowed to travel that distance
         elapsedTime,
         startTime,
         handleswipe = callback || function (swipedir) {};
