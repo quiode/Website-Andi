e8977a49774d821a084408f8a9cfe9aec672d0a4
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-24 12:05:27 +0200

    final version of renaming tool

--- heebphotography.ch/public/english/gallery/images/renaming.py
+++ heebphotography.ch/public/english/gallery/images/renaming.py
@@ -7,3 +7,5 @@ images_dir = input("Path to images:")
 all_images = [f for f in listdir(images_dir) if isfile(join(images_dir, f))]
 for counter, image in enumerate(all_images):
     rename(images_dir + image, images_dir + "image_" + str(counter) + ".jpg")
+    
+print("Finished!")
