2df2683fe31e6ff3c2da9233e2826cf4b10f2729
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-25 16:29:42 +0200

    testing dom php

--- heebphotography.ch/public/english/testing/Instagram/test2.php
+++ heebphotography.ch/public/english/testing/Instagram/test2.php
@@ -1,2 +1,13 @@
-$dom = new DOMDocument;
-$dom->load('https://www.instagram.com/heebphotography/');
+<!DOCTYPE html>
+<html>
+
+    <head></head>
+
+    <body>
+        <?php
+        $dom = new DOMDocument;
+        $dom->load('https://www.instagram.com/heebphotography/');
+        ?>
+    </body>
+
+</html>
