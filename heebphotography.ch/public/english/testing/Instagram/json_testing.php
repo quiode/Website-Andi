9d70dc75630091e25ad270f962877c758c0086f1
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-25 17:48:40 +0200

    testing

--- heebphotography.ch/public/english/testing/Instagram/json_testing.php
+++ heebphotography.ch/public/english/testing/Instagram/json_testing.php
@@ -5,8 +5,17 @@
 
     <body>
         <?php
-        $heebphotography_posts = json_decode("https://www.instagram.com/heebphotography/?__a=1");
-        print_r($heebphotography_posts);
+        // $heebphotography_posts = json_decode("https://www.instagram.com/heebphotography/?__a=1");
+        // print_r($heebphotography_posts);
+        // $regex = "/\"shortcode\":\".*\"/";
+        $file_contents = file_get_contents("https://www.instagram.com/heebphotography/?__a=1");
+        // echo "<h1>" . $file_contents . "</h1>";
+        // echo substr($file_contents, strpos($file_contents, "shortcode") + 12, 11);
+        phpinfo();
+        // while (substr_count($file_contents, "shortcode") > 0) {
+        //     echo substr($file_contents, strpos($file_contents, "shortcode") + 12, 11);
+        //     $file_contents = substr_replace($file_contents, "", strpos($file_contents, "shortcode") + 12, 11);
+        // }
         ?>
     </body>
 
