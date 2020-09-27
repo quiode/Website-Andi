57d56383d6672695ba53ef80fe49cff1129a06dd
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-27 22:57:44 +0200

    Update token_getter.php

--- heebphotography.ch/private/Instagram Acces Token/token_getter.php
+++ heebphotography.ch/private/Instagram Acces Token/token_getter.php
@@ -53,7 +53,7 @@ if (isset($_GET["code"])) { //get acces token
 
     // json file content
     echo "<h1>Json File Content</h1>";
-    file_put_contents("access_tokens.json", "{accessToken: " . $accesToken->getValue() . "}");
+    file_put_contents("access_tokens.json", '{"accessToken": "' . $accesToken->getValue() . '"}');
     file_put_contents("access_tokens.json", json_encode($accesToken->getExpiresAt(), FILE_APPEND));
     echo "<br>";
     print_r(json_decode(file_get_contents("access_tokens.json")));
