4df3b5942e7ab891126e35e90d729d24b04759cb
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-27 18:01:51 +0200

    *syntax

--- heebphotography.ch/public/english/contact/contact_backend.php
+++ heebphotography.ch/public/english/contact/contact_backend.php
@@ -44,7 +44,7 @@
         use PHPMailer\PHPMailer\PHPMailer;
         use PHPMailer\PHPMailer\Exception;
 
-        require "../../../../vendor/autoload.php"
+        require "../../../../vendor/autoload.php";
 //PHPMailer Object
         $mail = new PHPMailer(true); //Argument true in constructor enables exceptions
 //SMTP Server
