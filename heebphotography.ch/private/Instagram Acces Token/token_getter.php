89821a2b90d3944344eb7d641a5e6a8a4367ee29
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-27 22:29:11 +0200

    Update token_getter.php

--- heebphotography.ch/private/Instagram Acces Token/token_getter.php
+++ heebphotography.ch/private/Instagram Acces Token/token_getter.php
@@ -55,7 +55,7 @@ if (isset($_GET["code"])) { //get acces token
     echo "<h1>Json File Content</h1>";
     file_put_contents("access_tokens.json", json_encode((array)$accesToken));
     echo "<br>";
-    print_r(json_decode(file_get_contents("access_tokens.json")));
+    print_r((file_get_contents("access_tokens.json"));
 } else { //display login url
     $permissions = ["public_profile", "instagram_basic", "pages_show_list"];
     $loginUrl = $helper->getLoginUrl(FACEBOOK_REDIRECT_URI, $permissions);
