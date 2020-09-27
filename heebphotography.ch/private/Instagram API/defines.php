e68ec096f3101f23ad46e6a28c8caa70172d3312
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-28 00:59:00 +0200

    new url

--- heebphotography.ch/private/Instagram API/defines.php
+++ heebphotography.ch/private/Instagram API/defines.php
@@ -3,4 +3,4 @@
 
     define("FACEBOOK_APP_ID", "946808705814153");
     define("FACEBOOK_APP_SECRET", "5db3e7bd4a3e8bcb6783ab9b91e4e41b");
-    define("FACEBOOK_REDIRECT_URI", "https://heebphotography.ch/private/Instagram%20Acces%20Token/token_getter.php");
+    define("FACEBOOK_REDIRECT_URI", "https://heebphotography.ch/private/Instagram%20API/token_getter.php");
