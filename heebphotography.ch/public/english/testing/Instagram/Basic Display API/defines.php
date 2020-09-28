615307d49b7b45c0eef815e0c48e5b2f4d2d8b0c
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-28 18:37:16 +0200

    Update defines.php

--- heebphotography.ch/public/english/testing/Instagram/Basic Display API/defines.php
+++ heebphotography.ch/public/english/testing/Instagram/Basic Display API/defines.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
 
-    define("FACEBOOK_APP_ID", "326441455084963");
-    define("FACEBOOK_APP_SECRET", "ccbdb2198408d988cfd81a5e9cdb9f73");
+    define("FACEBOOK_APP_ID", "331617954597839");
+    define("FACEBOOK_APP_SECRET", "309d337cb250677a947917dec968b01f");
     define("FACEBOOK_REDIRECT_URI", "https://en.heebphotography.ch/testing/Instagram/Basic%20Display%20API/token_getter.php");
