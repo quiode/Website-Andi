7cef1af902f890d7f5c0bb04eebb8c2be29abc24
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-28 15:08:30 +0200

    Update html_getter.php

--- heebphotography.ch/private/Instagram API/html_getter.php
+++ heebphotography.ch/private/Instagram API/html_getter.php
@@ -29,7 +29,7 @@ function Ig_Page_Id_getter()
     if ($result != "false") {
         $result = json_decode($result, true);
 
-        var_dump($result["data"][0]);
+        var_dump($result["data"][0]["id"]);
         $page_id = $result["data"]["id"];
         curl_close($curl_session);
         return $page_id;
