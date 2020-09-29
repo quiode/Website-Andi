c7747b19cf6380127c67e5611c53a44c9b0384ce
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-09-29 19:32:59 +0200

    new mail mechanism

--- heebphotography.ch/public/english/contact/contact_backend.php
+++ heebphotography.ch/public/english/contact/contact_backend.php
@@ -7,11 +7,11 @@
 
     <body>
         <?php
-//shows errors/debugging
+        //shows errors/debugging
         ini_set('display_errors', '1');
         ini_set('display_startup_errors', '1');
         error_reporting(E_ALL);
-//makes the message which is send per mail in plain text
+        //makes the message which is send per mail in plain text
         $plain = ($_POST["first_name"] != "") ? strip_tags($_POST["first_name"]) : "NoFirstNameGiven";
         $plain .= " ";
         $plain .= ($_POST["last_name"] != "") ? strip_tags($_POST["last_name"]) : "NoLastNameGiven";
@@ -19,55 +19,55 @@
         $plain .= ($_POST["subject"] != "custom") ? strip_tags($_POST["subject"]) : strip_tags($_POST["custom_subject"]);
         $plain .= ". ";
         $plain .= "You can reach him via mail under " . strip_tags($_POST["email"]);
-//message of the email
-    //h1
+        //message of the email
+        //h1
         $message = "<h1>This message is automated!</h1>";
-    //h2
+        //h2
         $message .= "<h2>It was created through the Form on " . $_SERVER["HTTP_HOST"] . " by <b>";
         $message .= ($_POST["first_name"] != "") ? strip_tags($_POST["first_name"]) : "NoFirstNameGiven";
         $message .= " ";
         $message .= ($_POST["last_name"] != "") ? strip_tags($_POST["last_name"]) : "NoLastNameGiven";
         $message .= "</b>.</h2>";
-    //h3
+        //h3
         $message .= "<h3>The reason: <b>";
         $message .= ($_POST["subject"] != "custom") ? strip_tags($_POST["subject"]) : strip_tags($_POST["custom_subject"]);
         $message .= "</b>.</h3><h4>";
-    //h4
+        //h4
         $message .= "You can reach him via <b>" . "mail" . "</b> under <b>" . strip_tags($_POST["email"]) . "</b>.</h4>";
-    //h5
+        //h5
         $message .= "<h5>The following is the message from the user:</h5>";
-    //p
+        //p
         $message .= "<p>" . strip_tags($_POST["message"]) . "</p>";
-    //footer
+        //footer
         $message .= "<footer><b>Please do not reply to this message!</b></footer>";
-//imports all components
+        //imports all components
         use PHPMailer\PHPMailer\PHPMailer;
         use PHPMailer\PHPMailer\Exception;
 
         require __DIR__ . "/../../../../vendor/autoload.php";
-//PHPMailer Object
+        //PHPMailer Object
         $mail = new PHPMailer(true); //Argument true in constructor enables exceptions
-//SMTP Server
+        //SMTP Server
         $mail->SMTPAuth   = true;                  // enable SMTP authentication
         $mail->Host       = "smtp.gmail.com"; // sets the SMTP server
-        $mail->Port       = 587;                    // sets the SMTP port
-        $mail->SMTPSecure = 'tls';                  // TLS/SSL
+        $mail->Port       = 465;                    // sets the SMTP port
+        $mail->SMTPSecure = 'ssl';                  // TLS/SSL
         $mail->Username   = "contact.form.heebphotography@gmail.com"; // SMTP account username
-        $mail->Password   = "Yw63Bf#@uK@t%P7";        // SMTP account password
-//From email address and name
+        $mail->Password   = "J7lo92nfoVcb";        // SMTP account password
+        //From email address and name
         $mail->From = "contact.form.heebphotography@gmail.com";     //sender address
         $mail->FromName = "Contact Form";   //sender name
-//To address and name
+        //To address and name
         $mail->addAddress("andreas.heeb@heebphotography.ch"); //Recipient
-//Address to which recipient will reply
+        //Address to which recipient will reply
         $mail->addReplyTo("contact.form.heebphotography@gmail.com", "Contact Form");   //Replyier(?)
-//the actual content of the email
-    //sends html
+        //the actual content of the email
+        //sends html
         $mail->isHTML(true);
         $mail->Subject = "Contact Form: " . strip_tags($_POST["subject"]);
         $mail->Body = $message;
         $mail->AltBody = $plain . "&NewLine;The message:&NewLine;" . strip_tags($_POST["message"]);
-//sends the actual email
+        //sends the actual email
         try {
             $mail->send();
             // sends the status of the mail sending process to javascript
