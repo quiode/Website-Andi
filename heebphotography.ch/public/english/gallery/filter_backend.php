d3676acab1f51adee816695943241c1259d51f9a
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-10-05 19:34:28 +0200

    filter backend finished

--- heebphotography.ch/public/english/gallery/filter_backend.php
+++ heebphotography.ch/public/english/gallery/filter_backend.php
@@ -2,13 +2,12 @@
 session_start();
 if (sizeof($_SESSION["everything"]) == sizeof($_POST)) { // if everything was selected, change nothing
     $_SESSION["all"] = true;
-    echo "<script>window.close();</script>";
-} else {
-    foreach ($_SESSION["everything"] as $filter_option => $value) {
-        echo $value;
+} else { //adds all selected elements to the blacklist array
+    $_SESSION["blacklist_everything"] = array(); //removes everything from the blacklist
+    foreach ($_SESSION["everything"] as $filter_option) { //checks for every item that can be filtered, if its selected to be filtered
+        if (in_array($filter_option, $_POST)) {
+            array_push($_SESSION["blacklist_everything"], $filter_option);
+        }
     }
 }
-// testing
-var_dump($_SESSION);
-echo "<br>";
-var_dump($_POST);
+echo "<script>window.close();</script>";
