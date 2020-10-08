0e0f16c4e606fc0ac8143550e5d6cf333698a425
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-10-08 23:29:49 +0200

    testing

--- heebphotography.ch/public/english/gallery/gallery.js
+++ heebphotography.ch/public/english/gallery/gallery.js
@@ -6,11 +6,11 @@ document.addEventListener("keydown", key_pressed);
 function resizeToMax() {
     // myImage = new Image();
     var img = document.getElementById("slideshow_image");
-    if (img.src.width / document.body.clientWidth > img.src.height / document.body.clientHeight) {
-        img.style.width = "100%";
-    } else {
-        img.style.height = "100%";
-    }
+    // if (img.src.width / document.body.clientWidth > img.src.height / document.body.clientHeight) {
+    //     img.style.width = "100%";
+    // } else {
+    //     img.style.height = "100%";
+    // }
 
     // let the arrows fill the blank space
     var arrows_background = document.getElementsByClassName("arrows_background");
