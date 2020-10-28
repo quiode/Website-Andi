e02877ccc0b5bd1d7c2bc70bc908ed24b0ee7553
Quio <51075975+Noobmaster23@users.noreply.github.com>
2020-10-28 09:42:35 +0100

    Revert "Merge branch 'main' of https://github.com/Noobmaster23/Website-Andi into main"
    This reverts commit 56fbd301ccd60c1d947f30985f4762aa7b397ab8, reversing
    changes made to 4c24fa0d88349e2d1ce30e3deae15aa0cbecbc5a.

--- heebphotography.ch/public/english/templates/navigationbar.js
+++ heebphotography.ch/public/english/templates/navigationbar.js
@@ -7,40 +7,40 @@ function navigation_button(button) {
         // starts transformation --> element goes down
         navigation.style.height = "100%";
         // makes the cross black when the background gets whiter
-        // document.getElementById("b1").style.backgroundColor = "rgb(255,255,255)";
-        // document.getElementById("b1").style.border = "none";
-        // document.getElementById("b1").style.width = "72px";
-        // document.getElementById("b1").style.height = "12px";
+        document.getElementById("b1").style.backgroundColor = "rgb(255,255,255)";
+        document.getElementById("b1").style.border = "none";
+        document.getElementById("b1").style.width = "72px";
+        document.getElementById("b1").style.height = "12px";
 
-        // document.getElementById("b2").style.backgroundColor = "rgb(255,255,255)";
-        // document.getElementById("b2").style.border = "none";
-        // document.getElementById("b2").style.width = "72px";
-        // document.getElementById("b2").style.height = "12px";
+        document.getElementById("b2").style.backgroundColor = "rgb(255,255,255)";
+        document.getElementById("b2").style.border = "none";
+        document.getElementById("b2").style.width = "72px";
+        document.getElementById("b2").style.height = "12px";
 
-        // document.getElementById("b3").style.backgroundColor = "rgb(255,255,255)";
-        // document.getElementById("b3").style.border = "none";
-        // document.getElementById("b3").style.width = "72px";
-        // document.getElementById("b3").style.height = "12px";
+        document.getElementById("b3").style.backgroundColor = "rgb(255,255,255)";
+        document.getElementById("b3").style.border = "none";
+        document.getElementById("b3").style.width = "72px";
+        document.getElementById("b3").style.height = "12px";
 
         document.getElementsByTagName("body")[0].style.overflow = "hidden";
     } else {
         // starts transformation --> elements goes up
         navigation.style.height = "0%";
         // makes the cross white when the background gets darker
-        // document.getElementById("b1").style.backgroundColor = "rgb(0,0,0)";
-        // document.getElementById("b1").style.border = "1px #fff solid";
-        // document.getElementById("b1").style.width = "70px";
-        // document.getElementById("b1").style.height = "10px";
+        document.getElementById("b1").style.backgroundColor = "rgb(0,0,0)";
+        document.getElementById("b1").style.border = "1px #fff solid";
+        document.getElementById("b1").style.width = "70px";
+        document.getElementById("b1").style.height = "10px";
         
-        // document.getElementById("b2").style.backgroundColor = "rgb(0,0,0)";
-        // document.getElementById("b2").style.border = "1px #fff solid";
-        // document.getElementById("b2").style.width = "70px";
-        // document.getElementById("b2").style.height = "10px";
+        document.getElementById("b2").style.backgroundColor = "rgb(0,0,0)";
+        document.getElementById("b2").style.border = "1px #fff solid";
+        document.getElementById("b2").style.width = "70px";
+        document.getElementById("b2").style.height = "10px";
         
-        // document.getElementById("b3").style.backgroundColor = "rgb(0,0,0)";
-        // document.getElementById("b3").style.border = "1px #fff solid";
-        // document.getElementById("b3").style.width = "70px";
-        // document.getElementById("b3").style.height = "10px";
+        document.getElementById("b3").style.backgroundColor = "rgb(0,0,0)";
+        document.getElementById("b3").style.border = "1px #fff solid";
+        document.getElementById("b3").style.width = "70px";
+        document.getElementById("b3").style.height = "10px";
 
         document.getElementsByTagName("body")[0].style.overflow = "auto";
     }
