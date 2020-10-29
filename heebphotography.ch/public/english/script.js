274f52615c0fb7c88aa1e46b592c0e070cc14957
Quio <51075975+Noobmaster23@users.noreply.github.com>
2020-10-29 14:35:06 +0100

    undebug

--- heebphotography.ch/public/english/script.js
+++ heebphotography.ch/public/english/script.js
@@ -40,9 +40,6 @@ function picture_orientation() {
 		return "portrait_slides";
 	}
 
-	alert("test2");
-	alert(screen.orientation.type);
-
 	if (orientation == "portrait-primary" || orientation == "portrait-secondary") {
 		for (let index = 0; index < document.getElementsByClassName("portrait_slides").length; index++) {
 			const element = document.getElementsByClassName("portrait_slides")[index];
