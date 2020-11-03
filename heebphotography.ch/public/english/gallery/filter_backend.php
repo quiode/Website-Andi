f2d0acd76ae8d425767f540700e508faae090136
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-11-03 21:00:22 +0100

    debugging

--- heebphotography.ch/public/english/gallery/filter_backend.php
+++ heebphotography.ch/public/english/gallery/filter_backend.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+var_dump($_SESSION);
 if (sizeof($_SESSION["everything"]) == sizeof($_POST)) { // if everything was selected, change nothing
     $_SESSION["all"] = true;
 } else { //adds all selected elements to the blacklist array
