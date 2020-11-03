f372c1a378010f84480289ddd6f5b6303708d577
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-11-03 22:17:33 +0100

    everything button 1.1

--- heebphotography.ch/public/english/gallery/filter_backend.php
+++ heebphotography.ch/public/english/gallery/filter_backend.php
@@ -10,7 +10,17 @@ if (in_array("all", $_POST) and $all_first_clicked == "true") { // if the filter
     }
 } elseif (sizeof($_SESSION["everything"]) == sizeof($_POST) and !in_array("all", $_POST) and $all_first_clicked == "false") { // if the filter button was not clicked and also not selected, but everything else is selected, display everything
     $_SESSION["all"] = true;
+} elseif (sizeof($_SESSION["everything"]) == sizeof($_POST)-1 and in_array("all", $_POST) and $all_first_clicked == "false") { // if the filter button was not clicked but selected and everything else is selected, display everything
+    $_SESSION["all"] = true;
 } elseif (sizeof($_SESSION["everything"]) > sizeof($_POST) and !in_array("all", $_POST) and $all_first_clicked == "false") { // if the filter button was not clicked and not selected, but also not everything else was selected, display only that which was selected
+    $_SESSION["all"] = false; //not everything is selected
+    $_SESSION["blacklist"] = array(); //removes everything from the blacklist
+    foreach ($_SESSION["everything"] as $filter_option) { //checks for every item that can be filtered, if its selected to be filtered
+        if (!in_array($filter_option, $_POST)) {
+            array_push($_SESSION["blacklist"], $filter_option);
+        }
+    }
+} elseif (sizeof($_SESSION["everything"]) > sizeof($_POST)-1 and in_array("all", $_POST) and $all_first_clicked == "false") { // if the filter button was not clicked but selected and also not everything else was selected, display only that which was selected
     $_SESSION["all"] = false; //not everything is selected
     $_SESSION["blacklist"] = array(); //removes everything from the blacklist
     foreach ($_SESSION["everything"] as $filter_option) { //checks for every item that can be filtered, if its selected to be filtered
