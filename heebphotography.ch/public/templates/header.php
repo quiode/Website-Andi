e4c0d3dfc47b91bac38eea87e65ab3ccd9bf2c39
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-11-10 22:17:09 +0100

    debug

--- heebphotography.ch/public/templates/header.php
+++ heebphotography.ch/public/templates/header.php
@@ -15,6 +15,7 @@
         <div id="navigation-overlay">
             <div id="navigation-content">
                 <?php
+                var_dump(strpos($_SERVER['HTTP_HOST'], "en") != false);
                 if (strpos($_SERVER['HTTP_HOST'], "en") != false) {
                     echo "<a class=\"links\" href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "" . "\">Home</a>";
                     echo "<a class=\"links\" href=\"" ."https://" . $_SERVER['HTTP_HOST'] . "/" . "gallery/" . "\">Gallery</a>";
