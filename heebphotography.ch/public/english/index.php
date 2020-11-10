efe9f1eff60beda263912fa5a0591ef0f0a6a888
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-11-10 21:29:50 +0100

    no metatags, for now :)

--- heebphotography.ch/public/english/index.php
+++ heebphotography.ch/public/english/index.php
@@ -4,7 +4,6 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?php include __DIR__ . "/../templates/meta-tags.php"?>
         <title>Wildlifephotography Andreas Heeb</title>
         <link rel="stylesheet" href="https://heebphotography.ch/public/styles/main.css">
         <script src="https://heebphotography.ch/public/script/home.js"></script>
