c52fe23672768365640a07902426ec58f6f75472
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-11-10 22:43:53 +0100

    german about me

--- heebphotography.ch/public/deutsch/impressum/index.php
+++ heebphotography.ch/public/deutsch/impressum/index.php
@@ -4,48 +4,54 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Impressum | Wildlifephotography Andreas Heeb</title>
+        <title>Impressum | Tierfotografie Andreas Heeb</title>
         <link rel="stylesheet" href="https://heebphotography.ch/public/styles/main.css">
-        <link rel="shortcut icon" type="image/x-icon" href="https://heebphotography.ch/public/images/favicon/favicon.ico"/>
+        <link rel="shortcut icon" type="image/x-icon"
+            href="https://heebphotography.ch/public/images/favicon/favicon.ico" />
     </head>
 
     <body id="impressum">
         <!-- wip bar -->
         <?php require __DIR__ . "/../../templates/work_in_progress.php"?>
         <?php require __DIR__ . "/../../templates/header.php"?>
-        <section id="german" style="display: none;">
-            <p><strong>Kontakt-Adresse</strong></p>
-            <p>Dominik Schwaiger<br />Hofackerstrasse 3<br />8722 Kaltbrunn<br />Schweiz</p>
-            <p>E-Mail:<br />admin@heebphotography.ch</p>
-            <p>&nbsp;</p>
-            <p><strong>Haftungsausschluss</strong></p>Der Autor &uuml;bernimmt keinerlei Gew&auml;hr hinsichtlich
-            der inhaltlichen Richtigkeit, Genauigkeit, Aktualit&auml;t, Zuverl&auml;ssigkeit und
-            Vollst&auml;ndigkeit der Informationen.</p>
-            <p>Haftungsanspr&uuml;che gegen den Autor wegen Sch&auml;den materieller oder immaterieller Art, welche
-                aus dem Zugriff oder der Nutzung bzw. Nichtnutzung der ver&ouml;ffentlichten Informationen, durch
-                Missbrauch der Verbindung oder durch technische St&ouml;rungen entstanden sind, werden
-                ausgeschlossen.</p>
-            <p>Alle Angebote sind unverbindlich. Der Autor beh&auml;lt es sich ausdr&uuml;cklich vor, Teile der
-                Seiten oder das gesamte Angebot ohne besondere Ank&uuml;ndigung zu ver&auml;ndern, zu erg&auml;nzen,
-                zu l&ouml;schen oder die Ver&ouml;ffentlichung zeitweise oder endg&uuml;ltig einzustellen.</p>
-            <p>&nbsp;</p>
-            <p><strong>Haftungsausschluss f&uuml;r Links</strong></p>
-            <p>Verweise und Links auf Webseiten Dritter liegen ausserhalb unseres Verantwortungsbereichs. Es wird
-                jegliche Verantwortung f&uuml;r solche Webseiten abgelehnt. Der Zugriff und die Nutzung solcher
-                Webseiten erfolgen auf eigene Gefahr des jeweiligen Nutzers.</p>
-            <p>&nbsp;</p>
-            <p><strong>Urheberrechte</strong></p>
-            <p>Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf dieser
-                Website, geh&ouml;ren ausschliesslich <strong>Dominik Schwaiger, Andreas Heeb and Milena
-                    Bachofen</strong> oder den speziell genannten
-                Rechteinhabern. F&uuml;r die Reproduktion jeglicher Elemente ist die schriftliche Zustimmung des
-                Urheberrechtstr&auml;gers im Voraus einzuholen.</p>
-            <p>&nbsp;</p>
-            <!--ACHTUNG: Wenn Sie die Quelle ohne Erlaubnis von SwissAnwalt entfernen, dann begehen Sie eine Urheberrechtsverletzung welche in jedem Fall geahndet wird.--><br />Quelle:
-            <a href="https://www.swissanwalt.ch" target="_blank" rel="noopener">SwissAnwalt</a>
-            <!--Bitte beachten Sie die AGB von SwissAnwalt betreffend allf&auml;llig anfallenden Kosten bei Weglassen der Quelle!-->
+        <section id="german" class="impressum_text">
+            <article id="contact_address">
+                <p class="title">Kontakt-Adresse</p>
+                <p>Andreas Heeb<br />Bachtelstrasse 16<br />8640 Rapperswil-Jona<br />Schweiz</p>
+                <p>E-Mail:<br />andreas.heeb@heebphotography.ch</p>
+            </article>
+            <article id="disclaimer">
+                <p class="title">Haftungsausschluss</p>Der Autor &uuml;bernimmt keinerlei Gew&auml;hr hinsichtlich
+                der inhaltlichen Richtigkeit, Genauigkeit, Aktualit&auml;t, Zuverl&auml;ssigkeit und
+                Vollst&auml;ndigkeit der Informationen.</p>
+                <p>Haftungsanspr&uuml;che gegen den Autor wegen Sch&auml;den materieller oder immaterieller Art, welche
+                    aus dem Zugriff oder der Nutzung bzw. Nichtnutzung der ver&ouml;ffentlichten Informationen, durch
+                    Missbrauch der Verbindung oder durch technische St&ouml;rungen entstanden sind, werden
+                    ausgeschlossen.</p>
+                <p>Alle Angebote sind unverbindlich. Der Autor beh&auml;lt es sich ausdr&uuml;cklich vor, Teile der
+                    Seiten oder das gesamte Angebot ohne besondere Ank&uuml;ndigung zu ver&auml;ndern, zu erg&auml;nzen,
+                    zu l&ouml;schen oder die Ver&ouml;ffentlichung zeitweise oder endg&uuml;ltig einzustellen.</p>
+            </article>
+            <article id="exclusion_of_liability_for_links">
+                <p class="title">Haftungsausschluss f&uuml;r Links</p>
+                <p>Verweise und Links auf Webseiten Dritter liegen ausserhalb unseres Verantwortungsbereichs. Es wird
+                    jegliche Verantwortung f&uuml;r solche Webseiten abgelehnt. Der Zugriff und die Nutzung solcher
+                    Webseiten erfolgen auf eigene Gefahr des jeweiligen Nutzers.</p>
+            </article>
+            <article id="copyright">
+                <p class="title">Urheberrechte</p>
+                <p>Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf dieser
+                    Website, geh&ouml;ren ausschliesslich Dominik Schwaiger, Andreas Heeb and Milena
+                    Bachofen oder den speziell genannten
+                    Rechteinhabern. F&uuml;r die Reproduktion jeglicher Elemente ist die schriftliche Zustimmung des
+                    Urheberrechtstr&auml;gers im Voraus einzuholen.</p>
+                <!--ATTENTION: If you remove the source without permission from SwissAnwalt, you are committing a copyright infringement which will be punished in any case.-->
+                <br />Source:
+                <a href="https://www.swissanwalt.ch" target="_blank" rel="noopener">SwissAnwalt</a>
+                <!--Please note the terms and conditions of SwissAnwalt regarding any costs incurred if the source is omitted!-->
+            </article>
         </section>
-        <section id="english">
+        <section id="english" style="display: none;" class="impressum_text">
             <article id="contact_address">
                 <p class="title">Contact address</p>
                 <p>Andreas Heeb<br />Bachtelstrasse 16<br />8640 Rapperswil-Jona<br />Switzerland</p>
